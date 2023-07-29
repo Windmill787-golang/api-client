7d3ad8dad5ce16b7dea190cc31b7f749213b3f2c
Stolyarov Max <Windmill78max@gmail.com>
2023-07-29 16:13:32 +0300

    Instruction added, http client added, test api request added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Windmill787-golang/api-client
+
+go 1.20
--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"io"
+	"log"
+	"net/http"
+)
 
+type CatImage struct {
+	ID     string `json:"id"`
+	Url    string `json:"url"`
+	Width  string `json:"width"`
+	Height string `json:"height"`
+}
+
+var api_url = "https://api.thecatapi.com/v1/images/search"
+
+func logError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	//choose api with get, post methods and auth with token
+
+	//create struct with fields of entity
+	//create get and post funcs
+	//get will returns list of json objects
+	//get/:id will return single json object
+	//post will return success or error
+
+	//add auth func
+	resp, err := http.Get(api_url)
+	logError(err)
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	logError(err)
+
+	log.Println(string(body))
 }
